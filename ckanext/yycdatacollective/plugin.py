19fb0024d77ff01b096028eefc1df3717f02c705
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2018-04-03 22:46:14 -0400

    Add: Templating example

--- ckanext/yycdatacollective/plugin.py
+++ ckanext/yycdatacollective/plugin.py
@@ -1,13 +1,10 @@
+# encoding: utf-8
+
 import ckan.plugins as plugins
-import ckan.plugins.toolkit as toolkit
 
 
-class YycdatacollectivePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
+class ExampleThemePlugin(plugins.SingletonPlugin):
+    '''An example theme plugin.
 
-    # IConfigurer
-
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'yycdatacollective')
+    '''
+    pass
