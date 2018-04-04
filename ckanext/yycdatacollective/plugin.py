8b5092daf6abdcd2ee0da2d6a29f3f13c43bade7
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2018-04-03 21:12:03 -0400

    Add: Override default group_create to now allow anyone create a group

--- ckanext/yycdatacollective/plugin.py
+++ ckanext/yycdatacollective/plugin.py
@@ -3,13 +3,18 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+def group_create(context, data_dict=None):
+	return {'success': False, 'msg': 'No one is allowed to create groups'}
 
 class YycdatacollectivePlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IConfigurer)
-
+    # plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IAuthFunctions)
     # IConfigurer
 
-    def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'yycdatacollective')
+    # def update_config(self, config_):
+    #     toolkit.add_template_directory(config_, 'templates')
+    #     toolkit.add_public_directory(config_, 'public')
+    #     toolkit.add_resource('fanstatic', 'yycdatacollective')
+
+    def get_auth_functions(self):
+        return {'group_create': group_create}        
